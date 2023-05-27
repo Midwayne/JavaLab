c3818b48fab2587dfc7a3b7dec76adec01714b55
Kushal Krishna <kushal5070@gmail.com>
2023-05-27 23:04:23 +0530

    Add files via upload

---
+++ 2/Book.java
@@ -0,0 +1,19 @@
+package ans2;
+
+public class Book {
+	String title;
+	String author;
+	String publisher;
+	double price;
+	
+	public Book(String title, String author, String publisher, double price) {
+		this.title = title;
+		this.author = author;
+		this.publisher = publisher;
+		this.price = price;
+	}
+	
+	public String toString() {
+		 return "Book [title=" + title + ", author=" + author + ", publisher=" + publisher + ", price=" + price + "]";
+	}
+}
